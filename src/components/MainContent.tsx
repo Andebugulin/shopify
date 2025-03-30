cd8787009d61d09c3ae56baaeb055f6ec903a422
Andebugulin <gulin.andrey2005@gmail.com>
2025-03-30 22:27:24 +0300

    changed project to be vite based with typescript

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function MainContent() {
+    return (
+      <main>
+        <h2>Main Content Area</h2>
+        <p>This is where the main content goes.</p>
+      </main>
+    );
+  }
+  
